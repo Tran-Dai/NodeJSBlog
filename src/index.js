546a9749fe834e69860347fe7970decb5e4fb75c
= <=>
2021-02-08 20:48:30 +0700

    update project

--- src/index.js
+++ src/index.js
@@ -24,7 +24,16 @@ app.get("/", (req, res) => {
 });
 
 app.get("/news", (req, res) => {
+  console.log(req.query.q);
   res.render("news");
+});
+
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.post("/search", (req, res) => {
+  res.render("search");
 });
 
 app.listen(port, () => console.log("exaple app"));
